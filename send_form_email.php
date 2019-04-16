9e6bdeaec7b06fe10788750eff4eabe1fe18d262
unknown <cjmay@fredonia.edu>
2019-04-15 22:13:39 -0400

    test

--- send_form_email.php
+++ send_form_email.php
@@ -94,7 +94,8 @@ $headers = 'From: '.$sendtothisemail."\r\n".
     <title>Q&M Contracting</title>
   </head>
   <body>
-    <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #064F40;">
+
+   <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #064F40;">
   <a class="navbar-brand" href="#">Q&M Contracting</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -102,7 +103,7 @@ $headers = 'From: '.$sendtothisemail."\r\n".
   <div class="collapse navbar-collapse" id="navbarNavDropdown">
     <ul class="navbar-nav">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="contact.html">Contact</a>
@@ -126,35 +127,35 @@ $headers = 'From: '.$sendtothisemail."\r\n".
     </ul>
   </div>
 </nav>
-<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-  <ol class="carousel-indicators">
-    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-  </ol>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img class="d-block w-100" src="static/images/QandM.jpeg" alt="First slide">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="static/images/essjay2.png" alt="Second slide">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="static/images/GenieMc.jpg" alt="Third slide">
-    </div>
-  </div>
-  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div>
-<footer class="page-footer font-small blue">
 
-  <div id="footer" class="footer text-center py-3">Phone: (716)-826-1095 | Fax: (716-826-2100 | Email Adresss: QandM1799@roadrunner.com | Address: 1799 Seneca Street, Buffalo NY 14052
+    <!--The div element for the map -->
+    <div id="map"></div>
+    <script>
+// Initialize and add the map
+function initMap() {
+  // The location of Uluru
+  var uluru = {lat: 42.860770, lng: -78.817410};
+  // The map, centered at Uluru
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 15, center: uluru});
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({position: uluru, map: map});
+}
+    </script>
+    <!--Load the API from the specified URL
+    * The async attribute allows the browser to render the page while the API loads
+    * The key parameter will contain your own API key (which is not needed for this tutorial)
+    * The callback parameter executes the initMap() function
+    -->
+    <script async defer
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDuSZ-GHODSUDoyOfqP8LPhRoa5wSMFJ60&callback=initMap">
+    </script>
+
+<p id="contactSuccess">Your contact form has been successfully submitted!  We will review your submission and get back to you as soon as possible.  Thank you!</p>
+
+    <footer class="page-footer font-small blue">
+
+  <div id="footer" class="footer text-center py-3">Phone: (716)-826-1095 | Fax: (716)826-2100 | Email Adresss: QandM1799@roadrunner.com | Address: 1799 Seneca Street, Buffalo NY 14052
   </div>
 
 </footer>
@@ -163,6 +164,9 @@ $headers = 'From: '.$sendtothisemail."\r\n".
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+</div>
+ 
   </body>
 </html>
 <?php
