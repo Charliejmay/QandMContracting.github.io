865375d20dcf6f1676cd29d2f864837243cbb333
unknown <cjmay@fredonia.edu>
2019-04-15 20:32:29 -0400

    Changed email form

--- send_form_email.php
+++ send_form_email.php
@@ -29,6 +29,7 @@ if(isset($_POST['email'])) {
  
     $first_name = $_POST['first_name']; // required
     $last_name = $_POST['last_name']; // required
+    $email_from1 = $_POST['email_from']
     $telephone = $_POST['telephone']; // not required
     $comments = $_POST['comments']; // required
  
@@ -69,7 +70,7 @@ if(isset($_POST['email'])) {
  
     $email_message .= "First Name: ".clean_string($first_name)."\n";
     $email_message .= "Last Name: ".clean_string($last_name)."\n";
-    $email_message .= "Email: ".clean_string($email_from)."\n";
+    $email_message .= "Email: ".clean_string($email_from1)."\n";
     $email_message .= "Telephone: ".clean_string($telephone)."\n";
     $email_message .= "Comments: ".clean_string($comments)."\n";
  
